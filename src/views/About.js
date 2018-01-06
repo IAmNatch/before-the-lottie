3326d45197c51e489ad646fe98822e6169bf9187
Rebecca Vandop <>
2018-01-06 15:32:19 -0500

    working on about page design

--- src/views/About.js
+++ src/views/About.js
@@ -6,6 +6,12 @@ export const About = (props) => {
     }
 
     return (
+        <div>
+        <div className="title-holder">
+        <h1> About </h1>
+        <h2>Before the Plate</h2>
+        </div>
         <div class="block"></div>
+        </div>
     )
 }
