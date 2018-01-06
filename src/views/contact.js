f7f1563ad56f4cf9a3dc9e1364f5aea9e8171e77
IAmNatch <IAmNatch@users.noreply.github.com>
2018-01-06 17:37:00 -0500

    Added contact form to contact page!

--- src/views/contact.js
+++ src/views/contact.js
@@ -5,6 +5,14 @@ export const Contact = (props) => {
 
     }
     return (
-        <div>Contact!</div>
+        <div>
+            <form method="POST" action="http://formspree.io/info@sweettoothdigital.com">
+                <input type="name" name="name" placeholder="Name" />
+                <input type="email" name="email" placeholder="Email" />
+                <input type="subject" name="subject" placeholder="Subject" />
+                <textarea name="message" placeholder="Write your message here!"></textarea>
+                <button type="submit">Send</button>
+            </form>
+        </div>
     )
 }
