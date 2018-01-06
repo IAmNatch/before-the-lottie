5a556aee6006b17c9fce97b95a75813cd29abb85
Rebecca Vandop <>
2018-01-06 16:10:57 -0500

    about text added

--- src/views/About.js
+++ src/views/About.js
@@ -7,7 +7,11 @@ export const About = (props) => {
                 <h1> About </h1>
                 <h2>Before the Plate</h2>
             </div>
-            <div class="block"></div>
+            <div class="block">
+            <div className="Farmers">Farmers <p> Follow the people who dedicate their lives to growing the food we eat as we answer some of the most pressing questions about modern farming.</p></div>
+            <div className="Distributors">Distributors <p>See the logistics involved in getting food from the field to our plate. We will be featuring 100km Foods Inc, as well as the grocery chain Longo's.</p></div>
+            <div className="Restaurants">Restaurants <p>Experience cooking like never before as we feature head chef John Horne at the highly regarded restaurant Canoe, 54 floors up overlooking the city of Toronto.  </p> </div>
+            </div>
         </div>
     );
 };
