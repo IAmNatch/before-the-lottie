c12e65af9b2889bbc5e55ed4843204a96853a0cd
Rebecca Vandop <>
2018-01-10 14:16:06 -0500

    fucking instruction shit

--- src/components/lottie/LottieInstructions.js
+++ src/components/lottie/LottieInstructions.js
@@ -6,13 +6,21 @@ export const LottieInstructions = (props) => {
     if (props.view === 'home') {
         return (
             <div className='lottie-instructions animated fadeInRightBig'>
-                <p>Click on an</p>
+            <div className="test">
+                 <img src="./svg/up-arrow.svg"  />
+            </div>
+                <p>click on an</p>
                 <div></div>
                 <p>ingredient</p>
                 <div></div>
-                <p>to learn more</p>
+                <p>to learn</p>
                 <div></div>
-                <p>about its journey.</p>
+                <p>more about</p>
+                <div></div>
+                <p>its journey</p>
+                <div></div>
+                <p>to the plate</p>
+                
             </div>
         )
     } else {
