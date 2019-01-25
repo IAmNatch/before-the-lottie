341b699186ef58bf546c39b2f323ca0be720b2b3
Shane Robbins <>
2019-01-25 18:31:28 -0500

    analytics update

--- src/index.js
+++ src/index.js
@@ -11,12 +11,12 @@ import ReactGA from "react-ga";
 ReactGA.initialize("UA-133295797-1");
 const history = createHistory();
 
-ReactGA.set({ page: window.location.pathname });
-ReactGA.pageview(window.location.pathname);
+ReactGA.set({ page: window.location.pathname + window.location.search });
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 history.listen(location => {
-	ReactGA.set({ page: location.pathname });
-	ReactGA.pageview(location.pathname);
+	ReactGA.set({ page: location.pathname + location.search });
+	ReactGA.pageview(location.pathname + location.search);
 });
 
 // Location aware app...
